855e4c2f64e33090c0a028fe57a5ce02e22ae432
IT24102279 <IT24102279@my.sliit.lk>
2024-11-01 09:57:38 +0530

    Update IT24102279Lab3Q1A.java
    Added some comments and Tweaked the code to allow the user to buy rice in decimal values (eg 1.5kg)

--- Q1A/IT24102279Lab3Q1A.java
+++ Q1A/IT24102279Lab3Q1A.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
-public class IT24102279Lab3Q1 {
+public class IT24102279Lab3Q1A {
     
     public static void main(String[] args) {
         System.out.print("Enter the price of 1kg of rice : ");
-        Scanner input =  new Scanner(System.in);
-        double priceKg = input.nextDouble();
+        Scanner input =  new Scanner(System.in); //define scanner instance and namme it input)
+        double priceKg = input.nextDouble(); // get the double entered by the user and store it in a double
         
         Scanner input2 =  new Scanner(System.in);
         System.out.print("Enter the amount of rice you want in kg :");
-        int riceKg = input2.nextInt();
+        double riceKg = input2.nextDouble();
        
        System.out.println("Your bill is "+ riceKg*priceKg) ;
     }
