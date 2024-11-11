afa79453c94a360509970f650d7b4d492dd70e50
IT24102279 <IT24102279@my.sliit.lk>
2024-11-11 21:42:33 +0530

    Fix the messed up question answer
    i had commited the answer for Q2 in the Q3 folder before this commit fixes that issue

--- Q3/IT24102279Lab3Q3.java
+++ Q3/IT24102279Lab3Q3.java
@@ -1,24 +1,59 @@
 import java.util.Scanner;
-public class Main {
+
+public class IT24102279Lab3Q3{
     public static void main(String[] args) {
-        int salary, otHours, otRate; // declare variables 
-        float total;//since the question has a decimal point in the answer I used a float type here
+        int money, remainder, n5000, n1000, n500, n200, n100, n50, n20, n10, n5, n2, n1; //declare variables for money 
         
-        Scanner input1 = new Scanner(System.in);// input1, input2, input3 are scanner variables
-        System.out.print("Enter Salary ");
-        salary = input1.nextInt(); // geting the int from the user the next two are just like this
-        Scanner input2 = new Scanner(System.in);
-        System.out.print("Enter OT hours ");
-        otHours = input2.nextInt();
-        Scanner input3 = new Scanner(System.in);
-        System.out.print("Enter OT rate ");
-        otRate = input3.nextInt();
+        System.out.print("Enter the rupee amount");
+        Scanner input1 = new Scanner(System.in);
+        money = input1.nextInt();
+        
+        //n5000 means number of 5000 notes
+        n5000 = money / 5000;
+        remainder = money - n5000*5000;
+
+        n1000 = remainder / 1000;
+        remainder = remainder - n1000*1000;
+
+        n500 = remainder / 500;
+        remainder = remainder - n500*500;
+
+        n200 = remainder / 200;  // we don't use 200 notes in sri lanka anymore but since the question specifies I kept the 200 notes too
+        remainder = remainder - n200*200;
+
+        n100 = remainder / 100;
+        remainder = remainder - n100*100;
+        
+        n50 = remainder / 50;
+        remainder = remainder - n50*50;
+
+        n20 = remainder / 20;
+        remainder = remainder - n20*20;
+
+        n10 = remainder / 10;
+        remainder = remainder - n10*10;
+
+        n5 = remainder / 5;
+        remainder = remainder - n5*5;
+
+        n2 = remainder / 2;
+        remainder = remainder - n2*2;
+
+        // we do not need to do the remainder calcualtion for this last part
+        n1 = remainder / 1;
     
-        
-        total = salary + otHours*otRate; //does the salary total calculation
-        System.out.println("Your total salary is ");
-        System.out.println(total);
-        
-        
+
+        // Print the result for each denomination used s
+        System.out.println("5000 Notes – " + n5000);
+        System.out.println("1000 Notes – " + n1000);
+        System.out.println("500 Notes – " + n500);
+        System.out.println("200 Notes – " + n200);
+        System.out.println("100 Notes – " + n100);
+        System.out.println("50 Notes – " + n50);
+        System.out.println("20 Notes – " + n20);
+        System.out.println("10 Coins – " + n10);
+        System.out.println("05 Coins – " + n5);
+        System.out.println("02 Coins – " + n2);
+        System.out.println("01 Coins – " + n1);
     }
 }
